3a8975515f75e39c9447b8056292db41427cf9e5
Mohamed Amin Ben Chrifa <mohamed6aminbenchrifa@gmail.com>
2023-05-20 15:19:43 +0200

    finished subtitles functionality i guess

--- Assets/Scripts/SubtitlePlayer.cs
+++ Assets/Scripts/SubtitlePlayer.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class SubtitlePlayer : MonoBehaviour
 {
     [SerializeField] private TextAsset _subtitles;
+
+    public TextAsset Subtitles
+    {
+        get => _subtitles;
+        set => _subtitles = value;
+    }
     // Start is called before the first frame update
     void Start()
     {
         Subtitles subtitles = new Subtitles(_subtitles);
+
+        foreach (var subtitle in subtitles)
+        {
+            Debug.Log(subtitle.ToString());
+        }
     }
 
     // Update is called once per frame
